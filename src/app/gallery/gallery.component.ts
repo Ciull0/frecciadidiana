d5a9b4f933fb6d3a52b9576986daa20afba9d11a
Ciull0 <danieleciulli1997@gmail.com>
2021-04-12 23:37:04 +0200

    Added Text, Fixed Homescreen Reactiveness

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnChanges {
 
   @Input() data
   mainpic
   constructor() { }
 
   ngOnInit(): void {
+    this.mainpic = this.data.cover;
+  }
+
+  ngOnChanges():void{
     this.mainpic = this.data.cover;
   }
 
